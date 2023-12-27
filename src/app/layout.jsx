2c694ccf9048afb63c9bf843753d6c71e09b9da1
AsHim Shrestha <ashimsth89@gmail.com>
2023-12-27 22:46:46 +0545

    feat : add footer section

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,8 @@
-import './globals.css'
+import Footer from "@/components/Footer";
+import "./globals.css";
 import Head from "@/app/head";
 
-export default function RootLayout({children}) {
+export default function RootLayout({ children }) {
     return (
         <html lang="en">
         {/*
@@ -9,7 +10,12 @@ export default function RootLayout({children}) {
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
         <Head/>
-        <body>{children}</body>
+        <body className="relative min-h-screen">
+        {children}
+        {/* Footer */}
+
+        <Footer />
+        </body>
         </html>
-    )
+    );
 }
