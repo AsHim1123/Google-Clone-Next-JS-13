2bc410f88fb98b6817d800e46450fb1c51d142ec
AsHim Shrestha <ashimsth89@gmail.com>
2023-12-27 17:20:12 +0545

    feat : install tailwind css

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,6 @@
-import React from 'react'
+export default function Home() {
+    return (
+        <div>Home</div>
 
-const Home = () => {
-  return (
-    <div>
-      Home
-    </div>
-  )
+    )
 }
-
-export default Home
