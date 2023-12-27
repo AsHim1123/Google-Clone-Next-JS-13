4e4e724fdb810371ec6e1ae3ff2399fd6e641c5e
AsHim Shrestha <ashimsth89@gmail.com>
2023-12-27 18:44:03 +0545

    feat : add homeheader component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,12 @@
+import HomeHeader from "@/components/HomeHeader";
+
 export default function Home() {
     return (
-        <div>Home</div>
+
+        <>
+        <HomeHeader/>
+
+        </>
 
     )
 }
