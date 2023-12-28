2829571171179ec39bb7d913df9e78da95a72a80
AsHim Shrestha <ashimsth89@gmail.com>
2023-12-28 19:04:18 +0545

    chore : fetch data from google api

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
-const Page = () => {
+const Page = async() => {
+    const response = await fetch(` https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=lectures`);
+    const data = await response.json();
+    console.log(data)
+
     return (
-        <div>
+        <>
             Web Search Page
-        </div>
+        </>
     );
 };
 
