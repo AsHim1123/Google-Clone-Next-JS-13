a95cc0610e77511f53a4a76f6d98e4b225a66737
AsHim Shrestha <ashimsth89@gmail.com>
2023-12-28 22:54:01 +0545

    fix : handle possible errors

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
+import Link from "next/link";
 
 const WebSearchPage = async ({searchParams}) => {
     const response = await fetch(` https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`);
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch data')
+    }
+
     const data = await response.json();
-    console.log(data.items)
     const results = data.items;
+
+    if (!results) {
+        return <div className='flex flex-col justify-center items-center pt-10'>
+            <h1 className='text-3xl mb-4'>No Results Found!</h1>
+            <p className='text-lg '>Try Searching Something else or go back to homepage{" "}</p>
+            <Link className='text-blue-500' href='/'>
+                Home
+            </Link>
+
+        </div>
+
+
+    }
+
     return (
         <>
-            {results && results.map((result) =>
-                <h1>{result.title}</h1>
+            {results && results.map((result, i) =>
+                <h1 key={i}>{result.title}</h1>
             )}
 
         </>
