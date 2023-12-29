315cd374b2500f2c24f56f7923f2ad8b5cda3605
AsHim Shrestha <ashimsth89@gmail.com>
2023-12-29 18:40:29 +0545

    style : adjust padding for small size

--- src/components/ImageSearchResults.jsx
+++ src/components/ImageSearchResults.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import PaginationButtons from "@/components/PaginationButtons";
 
 const ImageSearchResults = ({results}) => {
-    return (<div className='pb-24 mt-4'>
+    return (<div className='sm:pb-24 pb-40 mt-4'>
         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-3 space-x-4'>
             {results.items.map((result) => (<div key={result.link} className='mb-8'>
                     <div className='group'>
