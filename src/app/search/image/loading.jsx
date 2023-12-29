30a21ad65bca3345202fcdbba282c21cd254ced0
AsHim Shrestha <ashimsth89@gmail.com>
2023-12-29 21:51:17 +0545

    style : add animating loading effect in image

--- src/app/search/image/loading.jsx
+++ src/app/search/image/loading.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Loading = () => {
     return (
-        <div>
-            Loading...
+        <div className='pt-10 mb-2 lg:pl-52 maax-w-6xl'>
+            <div className='animate-pulse'>
+                <div className='h-48 w-48 mb-4 bg-gray-200 rounded-md'></div>
+                <div className='h-2 w-48 mb-2.5 bg-gray-200 rounded-md'></div>
+                <div className='h-2 w-44 mb-2.5 bg-gray-200 rounded-md'></div>
+            </div>
         </div>
     );
 };
